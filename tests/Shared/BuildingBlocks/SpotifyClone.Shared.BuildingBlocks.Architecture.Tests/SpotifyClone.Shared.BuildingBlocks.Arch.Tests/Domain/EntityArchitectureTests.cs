af6ce37ffc20355af10beb526fbbe9c3ddb7e41d
denestrambola <tramboladenes@gmail.com>
2026-01-02 20:11:59 +0200

    feat: write architecture tests for Entities

---
+++ tests/Shared/BuildingBlocks/SpotifyClone.Shared.BuildingBlocks.Architecture.Tests/SpotifyClone.Shared.BuildingBlocks.Arch.Tests/Domain/EntityArchitectureTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using FluentAssertions;
+using NetArchTest.Rules;
+using SpotifyClone.Shared.BuildingBlocks.Domain.Primitives;
+
+namespace SpotifyClone.Shared.BuildingBlocks.Arch.Tests.Domain;
+
+public sealed class EntityArchitectureTests
+{
+    [Fact]
+    public void Entity_Should_BeAbstractClass()
+    {
+        // Arrange
+        Type entityType = typeof(Entity<,>);
+
+        // Act
+        bool isAbstractClass = entityType.IsAbstract && entityType.IsClass;
+
+        // Assert
+        isAbstractClass.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Entities_Should_BeSealed()
+    {
+        // Arrange
+        Assembly domainAssembly = typeof(Entity<,>).Assembly;
+
+        // Act
+        TestResult result = Types.InAssembly(domainAssembly)
+            .That()
+            .Inherit(typeof(Entity<,>))
+            .Should()
+            .BeSealed()
+            .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
