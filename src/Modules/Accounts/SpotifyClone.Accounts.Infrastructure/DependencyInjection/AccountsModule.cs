3e2101316e8fa04a88b2afa087f4d311914013fc
denestrambola <tramboladenes@gmail.com>
2026-01-10 17:30:32 +0200

    feat: register validators, mediatr handlers and dbcontexts in Accounts BC

---
+++ src/Modules/Accounts/SpotifyClone.Accounts.Infrastructure/DependencyInjection/AccountsModule.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SpotifyClone.Accounts.Application;
+using SpotifyClone.Accounts.Infrastructure.Persistence.Accounts;
+using SpotifyClone.Accounts.Infrastructure.Persistence.Identity;
+
+namespace SpotifyClone.Accounts.Infrastructure.DependencyInjection;
+
+public static class AccountsModule
+{
+    public static IServiceCollection AddAccountsModule(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddValidatorsFromAssembly(AccountsApplicationAssemblyReference.Assembly);
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(
+            AccountsApplicationAssemblyReference.Assembly));
+
+        services.AddDbContext<IdentityAppDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("MainDb")));
+
+        services.AddDbContext<AccountsAppDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("MainDb")));
+
+        return services;
+    }
+}
